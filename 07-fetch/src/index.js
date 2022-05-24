518a9afb95b81582b5f2b87edee6cdd8b9b6777f
ixnp <enprocopios@gmail.com>
2022-05-24 09:14:11 -0700

    fixed id

--- 07-fetch/src/index.js
+++ 07-fetch/src/index.js
@@ -1,41 +1,13 @@
 //Build header
 document.querySelector('h1').textContent = bookStore.name
-//Review 
-// Render name, address and store hours to footer
-console.log(bookStore)
+
+//Build footer
 document.querySelector('footer div').textContent = bookStore.name
-
 document.getElementById('address').textContent = bookStore.address
-
 document.querySelectorAll('footer div')[2].textContent = bookStore.hours
-// console.log(document.getElementsByTagName('div'))
-
-//Clear content from the DOM
+//Clear book list
 document.querySelector('#book-list li').remove()
-// document.querySelector('#book-list li').innerHTML = ''
-// document.querySelector('#book-list li').innerHTML = '<div>Rose is cute</div>'
-
-//1. Remove a book from the DOM with a click event
-// document.querySelector('h1').addEventListener('click', ()=>{
-//   console.log('hi')  
-// })
-
-//Adding Content with a form
-//2. Use a submit event to get data from a form and add it to the DOM
-// Are we using an Event?
-    // when DOM Loads 
-    // 
-// What Event are you using
-    // clicking on something
-    // submitting something 
-    // What action are we waiting for
-// Pick a target to trigger the event
-// Add the eventListener to the target 
-// pass the target the event and callback
-
-//Event: Submit 
-//Target: Form
-
+//render one book
 const renderOneBook = book => {
     const li = document.createElement('li')
     const h3 = document.createElement('h3')
@@ -60,10 +32,10 @@ const renderOneBook = book => {
 }
 
 document.querySelector('#book-form').addEventListener('submit',(e)=>{
+    //prevents the form from refreshing the browser
     e.preventDefault()
-    //ID
-    console.log(e.target["form-title"])
-    //Name
+
+    //event-param.what-the-event-listener-is-called-on.attribute-like-name-or-id.value
     const book = {
         title: e.target.title.value,
         author:e.target.author.value,
