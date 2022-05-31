a1f98d259958d5a5f4cb1a3885edda2635a2cf4f
ixnp <enprocopios@gmail.com>
2022-05-31 12:48:40 -0700

    solution

--- 08-post-patch-and-delete/src/index.js
+++ 08-post-patch-and-delete/src/index.js
@@ -24,6 +24,7 @@ const renderOneBook = book => {
     const pPrice = document.createElement('p')
     const img = document.createElement('img')
     const btn = document.createElement('button')
+    const inventoryInput = document.createElement('input')
 
     h3.textContent = book.title
     pAuthor.textContent = `Author: ${book.author}`
@@ -31,12 +32,18 @@ const renderOneBook = book => {
     btn.textContent = 'Delete'
 
     img.src = book.imageUrl
+    inventoryInput.value = book.inventory
+    inventoryInput.type = 'number'
     li.className = 'list-li'
 
     //Event Listeners 
-    btn.addEventListener('click',()=>li.remove())
+    inventoryInput.addEventListener('change', (e) => {
+        updateBook(e.target.value, book.id, e)
+    })
+
+    btn.addEventListener('click', (e) => deleteBook(book.id, e))
    
-    li.append(h3,pAuthor,pPrice,img,btn)
+    li.append(h3,pAuthor,pPrice,img,inventoryInput,btn)
     document.querySelector('#book-list').append(li)
 }
 
@@ -53,9 +60,10 @@ document.querySelector('#book-form').addEventListener('submit',(e)=>{
         inventory:e.target.inventory.value,
         reviews:[]
     }
-    renderOneBook(book)
+    createBook(book)
 })
 
+//READ: GET - 1
 const getStore = () => {
     fetch('http://localhost:3000/stores/1')
     .then((res) => res.json())
@@ -64,8 +72,9 @@ const getStore = () => {
         renderHeader(data)
         renderFooter(data)
     })
-    
+    .catch(e => console.log(e))
 }
+//READ: GET - all
 const getBooks = () => {
     fetch('http://localhost:3000/books')
     .then(res => res.json())
@@ -73,7 +82,62 @@ const getBooks = () => {
         console.log(inventory)
         inventory.forEach(renderOneBook)
     })
+    .catch(e => console.log(e))
 }
+
+
+
+// const getBooks = async () => {
+//     const res = await fetch('http://localhost:3000/books')
+//     const inventory = await res.json()
+//     inventory.forEach(renderOneBook)
+//}     
+//CREATE: POST
+const createBook = (book) => {
+    fetch('http://localhost:3000/books', {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(book)
+    })
+    .then(res => res.json())
+    .then(data => renderOneBook(data))
+    .catch(e => console.log(e))
+}
+
+//UPDATE: PATCH
+const updateBook = (data, bookID, e) => {
+    fetch(`http://localhost:3000/books/${bookID}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({inventory: data})
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(e => console.error(e))
+    // JSON.stringify({reviews:[...book.reviews, newBook.review]})
+   // .then(data => e.target.value = data.inventory)
+}
+
+//DELETE: DESTROY
+const deleteBook = (bookID, e) => {
+    fetch(`http://localhost:3000/books/${bookID}`,{
+        method:'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(() => {
+        e.target.parentNode.remove()
+    })
+    .catch(e => console.error(e))
+}
+
+
+
 getBooks()
 getStore()
 
